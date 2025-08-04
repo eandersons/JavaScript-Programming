c0b6b5f34b18146f9afbf61460554695a6182efa
Edgars <Edgars+GitHub@gaitenis.id.lv>
2025-08-04 10:31:13 -0400

    Add variable scope lab

---
+++ Scope/Scope Lab.js
@@ -0,0 +1,32 @@
+// Global scope
+var globalVar = "I am a global variable.";
+let globalLet = "I also am global, but scoped with `let`.";
+const globalConst = "I am a global constant.";
+
+{
+  // Block scope
+  var blockVar = "I am a block-scoped `var`.";
+  let blockLet = "I am a block-scoped `let`.";
+  const blockConst = "I am a block-scoped `const`.";
+}
+
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
+// console.log(blockVar);
+// console.log(blockLet);
+
+function show() {
+  var functionVar = "I am a block-scoped `var` in a function.";
+  let functionLet = "I am a block-scoped `let` in a function.";
+  const functionConst = "I am a block-scoped `const` in a function.";
+  console.log(functionVar);
+  console.log(functionLet);
+  console.log(functionConst);
+}
+
+show();
+
+// console.log(functionVar);
+// console.log(functionLet);
+// console.log(functionConst);
