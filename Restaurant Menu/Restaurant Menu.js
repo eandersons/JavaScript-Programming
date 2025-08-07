d8c7aaf77ee64b2885c47e6753a1ad9fc5398931
Edgars <Edgars+GitHub@gaitenis.id.lv>
2025-08-07 11:07:40 -0400

    Add prices to restaurant menu items

--- Restaurant Menu/Restaurant Menu.js
+++ Restaurant Menu/Restaurant Menu.js
@@ -1,18 +1,38 @@
-const breakfastMenu = ["Pancakes", "Eggs Benedict", "Oatmeal", "Frittata"];
-const mainCourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
-const dessertMenu = ["Cake", "Ice Cream", "Pudding", "Fruit Salad"];
+const breakfastMenu = [
+  ["Pancakes", 12],
+  ["Eggs Benedict", 23.45],
+  ["Oatmeal", 21.09],
+  ["Frittata", 15],
+];
+const currency = "€";
+const dessertMenu = [
+  ["Cake", 12.34],
+  ["Ice Cream", 6.66],
+  ["Pudding", 8.88],
+  ["Fruit Salad", 10.01],
+];
+const mainCourseMenu = [
+  ["Steak", 22.22],
+  ["Pasta", 13.57],
+  ["Burger", 15.51],
+  ["Salmon", 11.11],
+];
 document.getElementById("breakfastMenuItems").innerHTML = breakfastMenu
-  .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+  .map(
+    (item, index) =>
+      `<p>Item ${index + 1}: ${item[0]} (${currency}${item[1]})</p>`,
+  )
   .join("\n");
 let mainCourseItem = "";
 mainCourseMenu.forEach((item, index) => {
-  mainCourseItem += `<p>Item ${index + 1}: ${item}</p>\n`;
+  mainCourseItem += `<p>Item ${index + 1}: ${item[0]} (${currency}${item[1]})</p>\n`;
 });
 document.getElementById("mainCourseMenuItems").innerHTML = mainCourseItem;
 let dessertItem = "";
 
 for (let index = 0; index < dessertMenu.length; index++) {
-  dessertItem += `<p>Item ${index + 1}: ${dessertMenu[index]}</p>\n`;
+  const item = dessertMenu[index];
+  dessertItem += `<p>Item ${index + 1}: ${item[0]} (${currency}${item[1]})</p>\n`;
 }
 
 document.getElementById("dessertMenuItems").innerHTML = dessertItem;
