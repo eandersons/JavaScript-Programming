6fcd0e87fa7e21e88ce5ad355835e02ae80db678
Edgars <Edgars+GitHub@gaitenis.id.lv>
2025-08-08 12:27:54 -0400

    Add addition and division

--- Debug Code/Debug Code.js
+++ Debug Code/Debug Code.js
@@ -1,5 +1,17 @@
+function add(a, b) {
+  debugger;
+
+  return a + b;
+}
+
 function displayResult(result) {
   document.getElementById("result").textContent = `The result is: ${result}`;
+}
+
+function divide(a, b) {
+  debugger;
+
+  return a / b;
 }
 
 function multiply(a, b) {
@@ -13,7 +25,11 @@ function performOperation() {
   let num2 = parseInt(document.getElementById("input2").value);
 
   if (!isNaN(num1) && !isNaN(num2)) {
-    displayResult(multiply(num1, num2));
+    displayResult(
+      `addition - ${add(num1, num2)}; ` +
+        `multiplication - ${multiply(num1, num2)}; ` +
+        `division - ${divide(num1, num2)}`,
+    );
   } else {
     displayResult("Please enter valid numbers!");
   }
